f01379d2747c4e033046ec4ee0c23ba4bc3b7206
minhtq <minh.tranquang98@gmail.com>
2020-11-16 10:45:51 +0700

    update routes

--- age-verification.config.js
+++ age-verification.config.js
@@ -3,5 +3,5 @@ export const CHARGE_TIME = "monthly";
 export const PRICE = 1.99;
 export const APP_NAME = "Age Verification Popup";
 export const CHARGE_TITLE = "Age Verification Popup";
-export const HOST = process.env.HOST +  "/activate-charge";
+export const HOST = "https://minhlocal.omegatheme.com/activate_charge";
 // export const HOST = "https://f5fd1a9b869f.ngrok.io/check_charge";
