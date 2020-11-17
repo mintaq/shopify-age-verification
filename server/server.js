61367d2f392998ba85585b2c737711f44d6d9bc7
minhtq <minh.tranquang98@gmail.com>
2020-11-17 16:53:59 +0700

    remove set session

--- server/server.js
+++ server/server.js
@@ -37,15 +37,15 @@ app.prepare().then(() => {
   const server = new Koa();
   const router = new Router();
   server.use(cors());
-  server.use(
-    session(
-      {
-        sameSite: "none",
-        secure: true,
-      },
-      server
-    )
-  );
+  // server.use(
+  //   session(
+  //     {
+  //       sameSite: "none",
+  //       secure: true,
+  //     },
+  //     server
+  //   )
+  // );
   server.keys = [SHOPIFY_API_SECRET];
   server.use(
     shopifyAuth({
@@ -54,6 +54,7 @@ app.prepare().then(() => {
       scopes: [SCOPES],
 
       async afterAuth(ctx) {
+        console.log("ctx", ctx);
         const { shop, accessToken } = ctx.session;
 
         // CREATE/UPDATE SHOP AND ADD/UPDATE SCRIPT TO THEME
