503e8e98c21d72f2ab261b476e21c312170041e4
minhtq <minh.tranquang98@gmail.com>
2020-11-19 11:22:00 +0700

    try cookieless koa auth

--- pages/_app.js
+++ pages/_app.js
@@ -1,31 +1,18 @@
-// import ApolloClient from "apollo-boost";
-// import fetch from "node-fetch";
-// import { ApolloProvider } from "react-apollo";
-import App, { Container } from "next/app";
+import { Container } from "next/app";
 import { AppProvider } from "@shopify/polaris";
 import { Provider } from "@shopify/app-bridge-react";
-import Cookies from "js-cookie";
 import "@shopify/polaris/dist/styles.css";
 import translations from "@shopify/polaris/locales/en.json";
 import React, { useEffect, useState } from "react";
 
-// const client = new ApolloClient({
-//   fetch: fetch,
-// });
 const MyApp = ({ Component, pageProps }) => {
-  // const { Component, pageProps } = this.props;
-  // const shopOrigin = Cookies.get("shopOrigin");
   const [shopOrigin, set_shopOrigin] = useState("");
 
   useEffect(() => {
-    // console.log(window);
     if (window) {
       const { search } = window.location;
       const searchParams = new URLSearchParams(search);
-      // Get the `shop` (same as `shopOrigin`) URL parameter from the embedded app
       if (searchParams.has("shop")) {
-        // Redirect to the auth callback
-        // window.location = `/auth/inline?shop=${searchParams.get("shop")}`;
         set_shopOrigin(searchParams.get("shop"));
       }
     }
@@ -41,9 +28,7 @@ const MyApp = ({ Component, pageProps }) => {
             forceRedirect: true,
           }}
         >
-          {/* <ApolloProvider client={client}> */}
           <Component {...pageProps} shopOrigin={shopOrigin} />
-          {/* </ApolloProvider> */}
         </Provider>
       </AppProvider>
     </Container>
