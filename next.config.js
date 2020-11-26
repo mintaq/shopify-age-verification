b3d6c80e8f07a63ab2f233d926666eccc3d614e3
minhtq <minh.tranquang98@gmail.com>
2020-11-26 15:57:56 +0700

    update build path

--- next.config.js
+++ next.config.js
@@ -4,16 +4,9 @@ const webpack = require("webpack");
 const apiKey = JSON.stringify(process.env.SHOPIFY_API_KEY);
 
 module.exports = {
-  // assetPrefix: "/age-verification",
-  // async rewrites() {
-  //   return [
-  //     {
-  //       source: "/age-verification/_next/:path*",
-  //       destination: "/_next/:path*",
-  //     },
-  //   ];
-  // },
   basePath: "/age-verification",
+  assetPrefix: "/age-verification/nextjs-static/",
+
   webpack: (config) => {
     const env = { API_KEY: apiKey };
     config.plugins.push(new webpack.DefinePlugin(env));
